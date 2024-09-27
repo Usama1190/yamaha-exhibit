c5e05b91010ad436b6edd9f2335bd73e89cccb88
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2024-09-26 17:22:18 -0700

    Keep working on Sep 27, 24

--- utils/data.js
+++ utils/data.js
@@ -1,17 +1,32 @@
 export const data = [
     {
         id: 1,
-        img1: '',
-        img2: '',
+        img1: '/images/bike1-removebg-preview.png',
+        img2: '/images/bike2-removebg-preview (1).png',
     },
     {
-        id: 1,
-        img1: '',
-        img3: '',
+        id: 2,
+        img1: '/images/bike1-removebg-preview.png',
+        img3: '/images/bike3-removebg-preview.png',
     },
     {
-        id: 1,
-        img1: '',
-        img4: '',
+        id: 3,
+        img1: '/images/bike1-removebg-preview.png',
+        img4: '/images/bike4-removebg-preview.png',
+    },
+    {
+        id: 4,
+        img1: '/images/bike1-removebg-preview.png',
+        img4: '/images/bike5-removebg-preview.png',
+    },
+    {
+        id: 5,
+        img1: '/images/bike1-removebg-preview.png',
+        img4: '/images/bike6-removebg-preview.png',
+    },
+    {
+        id: 6,
+        img1: '/images/bike1-removebg-preview.png',
+        img4: '/images/bike7-removebg-preview.png',
     }
 ];
